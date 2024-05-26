81ad233f9d8cd571d19cf589e3fdca0afc44a3c0
LeslT <lesliejailine@hotmail.com>
2024-05-26 18:30:12 -0500

    add auth to update user

--- backend-pr/src/routes/userRoutes.js
+++ backend-pr/src/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import userController from '../controllers/userController.js'
+import auth from '../middlewares/auth.js'
 
 const router = express.Router()
 
-router.patch('/:email', userController.updateUserByEmail)
+router.patch('/:email', auth, userController.updateUserByEmail)
 
 export default router
